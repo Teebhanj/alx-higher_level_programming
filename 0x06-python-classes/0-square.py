4d09a25938889b28f789dbae771843ba799e7bc9
Teebhanj <oyebanjioluwatosin1@gmail.com>
2023-03-29 14:35:23 -0700

    class created

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-"""Define a class, Square"""
+"""Define a class, Square."""
 
 
 class Square:
-    """Pass statment since we are defining an empty class"""    
+    """Pass statment since we are defining an empty class."""
     pass
