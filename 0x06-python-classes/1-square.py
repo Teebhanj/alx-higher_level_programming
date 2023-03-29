17e2cbb064479605305d10a2dd860407e442a0e3
Teebhanj <oyebanjioluwatosin1@gmail.com>
2023-03-29 15:07:09 -0700

    private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Define a class, Square."""
+
+
+class Square:
+    """Define attribute of Square instance"""
+
+    def __init__(self, size):
+        """Initialize private attribute size"""
+
+        self.__size = size
